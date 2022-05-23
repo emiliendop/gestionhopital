6ffdbb5bbc8bb6200e7f77c16cc8a893ea59f0cc
emiliendop <emiliendop10@gmail.com>
2022-05-23 23:16:56 +0100

    second commit

---
+++ view/login.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="index1.php" method="">
+Identifiant: <input type="text" name="idf" >
+
+            Mot de passe : <input type="password" name="mdp" >
+       
+        <input type="submit" name="envoyer" value="Envoyer">
+</form>
+</body>
+</html>
